4a1c2c742a3acd48bde6e96ae373704fbbb2cb50
KLM-39 <101316694+KLM-39@users.noreply.github.com>
2022-06-20 10:59:27 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://12950e4f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
